93b984678508b8726bca3205c14548fd80f603d8
virajpate <virajpate27@gmail.com>
2020-12-26 11:37:50 +0530

    display movie list4

--- app/src/main/java/com/example/movielistapp/MainActivity.java
+++ app/src/main/java/com/example/movielistapp/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
                 adapter=new MoviesAdapter(movies);
                 moviesList.setAdapter(adapter);
 
+
             }
 
             @Override
